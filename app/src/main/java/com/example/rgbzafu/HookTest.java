f3eed2b9e29b801b7d5ec90d7876953d513ab97e
tianxitx <69033238+tianxitx@users.noreply.github.com>
2023-05-13 17:15:09 +0800

    Update HookTest.java

--- app/src/main/java/com/example/rgbzafu/HookTest.java
+++ app/src/main/java/com/example/rgbzafu/HookTest.java
@@ -34,7 +34,7 @@ import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 
 public class HookTest implements IXposedHookZygoteInit,IXposedHookInitPackageResources {
     private static String MODULE_PATH = null;
-    private TextView mContainer;
+
     int c = Color.rgb(255, 183, 197);
 
     public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
@@ -52,28 +52,5 @@ public class HookTest implements IXposedHookZygoteInit,IXposedHookInitPackageRes
         resparam.res.setReplacement("edu.zafu.uniteapp", "mipmap", "menu5_active", modRes.fwd(R.mipmap.menu5_p));
         resparam.res.setReplacement("edu.zafu.uniteapp", "mipmap", "menu6_active", modRes.fwd(R.mipmap.menu6_p));
         resparam.res.setReplacement("edu.zafu.uniteapp", "mipmap", "logo_index", modRes.fwd(R.mipmap.dog));
-        XposedBridge.log("layout  resNames.fullname:");
-        resparam.res.hookLayout("edu.zafu.uniteapp", "layout", "fragment_contact", new XC_LayoutInflated() {
-            @Override
-            public void handleLayoutInflated(XC_LayoutInflated.LayoutInflatedParam liparam) throws Throwable {
-                XposedBridge.log("layout  resNames.fullname:" + liparam.resNames.fullName);
-                XposedBridge.log("layout  resNames.id:" + liparam.resNames.id);
-                XposedBridge.log("layout  resNames.name:" + liparam.resNames.name);
-                XposedBridge.log("layout  resNames.pkg:" + liparam.resNames.pkg);
-                XposedBridge.log("layout  resNames.type:" + liparam.resNames.type);
-                XposedBridge.log("layout  resNames.variant:" + liparam.variant);
-                XposedBridge.log("layout  resNames.view:" + liparam.view);
-
-                ConstraintLayout simpleItem = (ConstraintLayout) liparam.view;
-                TextView sexTextView = new TextView(simpleItem.getContext());
-                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT);
-                sexTextView.setBackgroundColor(Color.rgb(0,0,0));
-                sexTextView.setLayoutParams(params);
-                sexTextView.setText("color");
-                simpleItem.addView(sexTextView, 1, params);
-            }
-        });
     }
 }
