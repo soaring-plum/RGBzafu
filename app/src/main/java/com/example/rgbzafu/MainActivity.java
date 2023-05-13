3a78a5c1a087a7ff5db37285b3471db2a7747c36
tianxitx <69033238+tianxitx@users.noreply.github.com>
2023-05-13 17:15:49 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/rgbzafu/MainActivity.java
+++ app/src/main/java/com/example/rgbzafu/MainActivity.java
@@ -27,23 +27,12 @@ import java.io.OutputStreamWriter;
 import java.sql.DriverPropertyInfo;
 
 public class MainActivity extends AppCompatActivity {
-    private ConstraintLayout mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-        mContainer = findViewById(R.id.cl1);
-//        LinearLayout simpleItem = new LinearLayout(this);
-        TextView sexTextView = new TextView(this);
-//        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams();
-//        params.leftMargin = 10;
-//        sexTextView.setLayoutParams(params);
-        sexTextView.setBackgroundColor(Color.rgb(255, 0, 0));
-        sexTextView.setText("color");
-        sexTextView.setTextSize(30);
-        sexTextView.setTextColor(Color.rgb(255, 255, 255));
-        mContainer.addView(sexTextView, 1);
+        mContainer = findViewById(R.id.activity_main);
     }
 }
